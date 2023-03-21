763ebc073afb303723f9b8608f95fb9a9f866e58
danny <danny.smargiassi@gmail.com>
2023-03-21 17:43:27 -0400

    implemented translate function

--- ChatbotMain.py
+++ ChatbotMain.py
@@ -1,13 +1,14 @@
 import os
 import discord
-from discord.ext import commands
-from dotenv import load_dotenv
 import translate
-import botCommands
+from dotenv import load_dotenv
+
 load_dotenv()
 token = os.getenv('TOKEN')
+
 intents = discord.Intents.default()
 intents.message_content = True
+
 class DevPSUBot(discord.Client):
     async def on_ready(self):
         print("logged on!")
@@ -27,17 +28,34 @@ class DevPSUBot(discord.Client):
             print("react command recognized")
             await message.add_reaction("👍")
             await message.add_reaction("👽")
-        # Implementation of google translate feature
-        if "translate" in message.content.lower():
-            print("command recognized: translate")
-            #await message.channel.send()
-        # Implementation of commands
-     #   bot = commands.Bot(command_prefix='!', intents=intents)
-        # Have it pass into the class "Commands" if the prefix is found
 
+#Transalate.
 
+        
+        if 'translate' in message.content.lower():
+            message_list = message.content.split()
+            i = 1
+            msg = ''
 
-    
+            # translate (string) to (language)
+            if 'to' in message.content:
+                while message_list[i] != 'to':
+                    msg += message_list[i]
+                    i += 1
+                
+                lang = message_list[i+1]
+
+                await message.channel.send(translate.translator_func(msg, lang))
+
+            # translate (string)
+            else:
+                while i < len(message_list):
+                    msg += message_list[i]
+                    i += 1
+            
+                await message.channel.send(translate.translator_func(msg))
+
+            
     async def on_typing(self, channel, user, when):
         print(f"{user} is typing in {channel} at {when}")
         await channel.send(f"i see you typing, {user}")
